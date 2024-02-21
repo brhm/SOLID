2edbccfac6b1ec631bd5992be9d678f53e693ed3
ibrahim keskin <brhmkskn@outlook.com>
2024-02-21 13:39:04 +0100

    Created a class that has many responsibilities, -write to console, -store data -insert/update/delete operations

---
+++ SRP.App/SRP.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP.App
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        private static List<Product> ProductList = new List<Product>();
+        public static List<Product> GetProducts=> ProductList; // get property =>  GetProducts{ get{return ProductList;}}
+        public Product()
+        {
+            ProductList = new()
+            {
+                new Product {Id=1,Name="Kalem 1"},
+                new Product {Id=2,Name="Kalem 2"},
+                new Product {Id=3,Name="Kalem 3"},
+                new Product {Id=4,Name="Kalem 4"},
+                new Product {Id=5,Name="Kalem 5"}
+            };
+
+        }
+        public void SaveOrUpdate(Product product)
+        {
+            var hasProduct= ProductList.Any(p=>p.Id==product.Id);
+
+            if(!hasProduct)
+            {
+                ProductList.Add(product);
+            }
+            else
+            {
+                var index= ProductList.FindIndex(p=>p.Id==product.Id);
+
+                ProductList[index] = product;
+            }
+        }
+        public void Detele(int  id)
+        {
+            var hasProduct=ProductList.Find(p=>p.Id==id);
+
+            if(hasProduct==null) {
+
+                throw new Exception($"{id} idli Ürün Bulunamadı");
+
+            }
+
+                ProductList.Remove(hasProduct);
+        }
+        public void WriteToConsole()
+        {
+            ProductList.ForEach(p =>
+            {
+                Console.WriteLine($"{p.Id} - {p.Name}");
+            });
+            Console.ReadLine();
+        }
+
+    }
+
+}
