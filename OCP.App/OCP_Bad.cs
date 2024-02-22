83d28c563108c3b1b44fe02db82e3463b08fbe7f
ibrahim keskin <brhmkskn@outlook.com>
2024-02-22 14:41:47 +0100

    Bad OCP codes

---
+++ OCP.App/OCP_Bad.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCP.App
+{
+    public class SalaryCalculate
+    {
+        public decimal Calculate(decimal salary,SalaryType salaryType)
+        {
+            //This code depends on the enums and switch case. 
+            //In the future we may want to change the enum list, add a new enum value, or change to calculate operations. all of those requirements affect all codes. we will need to change many things. 
+            //We will need to change unit tests and the other dependent code as well
+
+            decimal newSalary = 0;
+            switch (salaryType)
+            {
+                case SalaryType.Low:
+                    newSalary = salary * 2;
+                    break;
+                case SalaryType.Middle:
+                    newSalary = salary * 4;
+                    break;
+                case SalaryType.High:
+                    newSalary = salary * 6;
+                    break;
+                default:
+                    break;
+            }
+            return newSalary;
+        }
+
+    }
+   public enum SalaryType
+    {
+        Low,
+        Middle,
+        High
+    }
+
+}
---
+++ OCP.App/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OCP.App;
+
+Console.WriteLine("Hello, World!");
+
+SalaryCalculate calculate = new SalaryCalculate();
+
+Console.WriteLine($"Low Salary:{calculate.Calculate(100, SalaryType.Low)}");
+Console.WriteLine($"Middle Salary:{calculate.Calculate(100, SalaryType.Middle)}");
+Console.WriteLine($"High Salary:{calculate.Calculate(100, SalaryType.High)}");
+
+Console.ReadLine();
