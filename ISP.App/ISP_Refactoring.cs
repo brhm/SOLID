b3f42677a7a4a602ad5b2405d78061c36081d2bd
ibrahim keskin <brhmkskn@outlook.com>
2024-02-23 14:43:49 +0100

    bad approach

---
+++ ISP.App/ISP_Refactoring.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISP.App.Good
+{
+    /// <summary>
+    ///  class library => Read imp
+    ///  class library => Create/Update/Delete imp
+    /// </summary>
+    /// 
+
+    // 1. Read Repository
+    public class ReadRepository : IProductRepository
+    {
+        // Create, DeleteById, Update functions is not necessary here. We have to avoid that.
+        public void Create(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public interface IProductRepository
+    {
+        Product GetById(int id);
+        List<Product> GetAll();
+
+        void DeleteById(int id);
+        void Update(Product product);
+        void Create(Product product);
+    }
+
+
+}
