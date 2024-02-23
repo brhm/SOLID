c175a29d6f00959f503c884d8504dd9a3f40938e
ibrahim keskin <brhmkskn@outlook.com>
2024-02-23 14:13:56 +0100

    LSP bad Example

---
+++ LSP.App/LSP_Bad.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSP.App.Bad
+{
+    // abstract 
+    public abstract class BasePhone
+    {
+        public void Call()
+        {
+            Console.WriteLine("Call Phone");
+        }
+
+        public abstract void TakePhoto();
+    }
+
+    public class IPhone : BasePhone
+    {
+        public override void TakePhoto()
+        {
+            Console.WriteLine("Take photo");
+        }
+    }
+    public class Nokia3310 : BasePhone
+    {
+        public override void TakePhoto()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LSP.App/Program.cs
@@ -0,0 +1,17 @@
+﻿using LSP.App.Bad;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+BasePhone phone = new IPhone();
+
+phone.Call();
+phone.TakePhoto();
+
+Console.WriteLine("---------------------");
+
+phone=new Nokia3310();
+phone.Call();
+phone.TakePhoto(); // Nokia3310 doesn't have this method,when we run it, it throw exception. 
+// The advantage that LSP gives us is to avoid and prevent such mistakes.
+// When we create an instance from Nokia 3310, we mustn't use Take Photo. For that, We will apply the LSP to our codes.
+Console.ReadLine();
