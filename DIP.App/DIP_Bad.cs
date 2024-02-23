193fa29c2e178cdd57012c21c4ffcd5007bc94d3
ibrahim keskin <brhmkskn@outlook.com>
2024-02-23 15:05:39 +0100

    Dependency inversion princible bad example

---
+++ DIP.App/DIP_Bad.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIP.App.Bad
+{
+    public class ProductService
+    {
+        // High-level class (service) dependent on the low-level class.
+        private readonly ProductRepositoryFromSQLServer _repository;
+        public ProductService(ProductRepositoryFromSQLServer repository)
+        {
+                _repository = repository;
+        }
+        public List<string> GetAll()
+        {
+            return _repository.GetAll();
+        }
+    }
+
+    public class ProductRepositoryFromSQLServer
+    {
+        public List<string> GetAll()
+        {
+            return new List<string> { "SQL Server Kalem", "SQL Server Defter", "SQL Server Kitap" };
+        }
+    }
+}
---
+++ DIP.App/Program.cs
@@ -0,0 +1,10 @@
+﻿using DIP.App.Bad;
+// See https://aka.ms/new-console-template for more information
+
+// dependency inversion princible.
+
+var productService = new ProductService(new ProductRepositoryFromSQLServer());
+productService.GetAll().ForEach(p=> Console.WriteLine(p));
+
+
+Console.ReadLine();
